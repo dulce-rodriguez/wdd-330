2ea6dc0e6747e21e8aa7f8e5be5e5eee2f8ff596
dulce-rodriguez <83044978+dulce-rodriguez@users.noreply.github.com>
2022-04-28 18:43:24 -0600

    Week 2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const links = [
     {
       label: "Week 1 notes",
-      url: "week1/index.html"
+      url: "week1/index.html",
+      label: "Week 2 notes",
+      url: "week2/index.html"
     }
   ]
 
